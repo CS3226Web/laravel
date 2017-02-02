583ddd23badd7930a1269fa28a6790d1060f6698
hungbie <a0112086@u.nus.edu>
2017-02-02 19:03:48 +0800

    Update StudentController.php

--- Controller/StudentController.php
+++ Controller/StudentController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class SampleController extends Controller
+class StudentController extends Controller
 {
     public function index() {return view('index'); }
     public function detail($id) {return view('detail')->with('id', $id); }
