7ceae846878c6979b27d5d97cfb1d3b0729ea604
hungbie <a0112086@u.nus.edu>
2017-02-02 18:31:04 +0800

    Create StudentController.php

---
+++ Controller/StudentController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SampleController extends Controller
+{
+    public function index() {return view('index'); }
+    public function detail(id) {return view('detail')->with('id', id); }
+}
+?>
